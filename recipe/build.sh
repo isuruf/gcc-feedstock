0a265eb3bb6cfd44c8a608c20489862c260704a5
Isuru Fernando <isuruf@gmail.com>
2020-01-28 21:36:19 +0000

    Add gcc

--- recipe/build.sh
+++ recipe/build.sh
@@ -2,29 +2,22 @@
 
 set -e
 
+#for file in ./crosstool_ng/packages/gcc/$PKG_VERSION/*.patch; do
+#  patch -p1 < $file;
+#done
 
-#pushd ${BUILD_PREFIX}/bin
-#  for fn in "${BUILD}-"*; do
-#    new_fn=${fn//${BUILD}-/}
-#    echo "Creating symlink from ${fn} to ${new_fn}"
-#    ln -sf "${fn}" "${new_fn}"
-#    varname=$(basename "${new_fn}" | tr a-z A-Z | sed "s/+/X/g" | sed "s/\./_/g" | sed "s/-/_/g")
-#    echo "$varname $CC"
-#    printf -v "$varname" "$BUILD_PREFIX/bin/${new_fn}"       
-#  done
-#popd
+export HOST="${ctng_cpu_arch}-${ctng_vendor}-linux-gnu"
 
-for file in ./crosstool_ng/packages/binutils/$PKG_VERSION/*.patch; do
+pushd binutils
+for file in ../crosstool_ng/packages/binutils/2.29.1/*.patch; do
   patch -p1 < $file;
 done
 
 mkdir build
 cd build
 
-export HOST="${ctng_cpu_arch}-${ctng_vendor}-linux-gnu"
-
 ../configure \
-  --prefix="$PREFIX" \
+  --prefix="$BUILD_PREFIX" \
   --target=$HOST \
   --enable-ld=default \
   --enable-gold=yes \
@@ -35,6 +28,49 @@ export HOST="${ctng_cpu_arch}-${ctng_vendor}-linux-gnu"
   --disable-nls \
   --enable-default-pie \
   --with-sysroot=$PREFIX/$HOST/sysroot \
+  --with-build-sysroot=$PREFIX/$HOST/sysroot
+
+make -j${CPU_COUNT}
+make install
+
+for f in addr2line ar as c++filt dwp elfedit gprof ld ld.bfd ld.gold nm objcopy objdump ranlib readelf size strings strip; do
+    ln -s $BUILD_PREFIX/bin/x86_64-conda-linux-gnu-$f $BUILD_PREFIX/bin/$f
+done
+popd
+
+./contrib/download_prerequisites
+
+mkdir build
+cd build
+
+export HOST="${ctng_cpu_arch}-${ctng_vendor}-linux-gnu"
+
+../configure \
+  --prefix="$PREFIX" \
+  --libdir="$PREFIX/lib" \
+  --target=$HOST \
+  --enable-default-pie \
+  --enable-languages=c,c++,fortran,objc,obj-c++ \
+  --enable-__cxa_atexit \
+  --disable-libmudflap \
+  --enable-libgomp \
+  --disable-libssp \
+  --enable-libquadmath \
+  --enable-libquadmath-support \
+  --enable-libsanitizer \
+  --enable-libmpx \
+  --enable-lto \
+  --with-host-libstdcxx='-static-libgcc -Wl,-Bstatic,-lstdc++,-Bdynamic -lm' \
+  --enable-threads=posix \
+  --enable-target-optspace \
+  --enable-plugin \
+  --enable-gold \
+  --disable-nls \
+  --disable-multilib \
+  --enable-long-long \
+  --enable-default-pie \
+  --with-sysroot=$PREFIX/$HOST/sysroot \
+  --with-build-sysroot=$PREFIX/$HOST/sysroot
 
 make -j${CPU_COUNT}
 
